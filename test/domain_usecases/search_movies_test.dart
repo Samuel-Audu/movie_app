1377c4758fa6647c782c5e1afac839e36fc69665
Samuel Audu <mrsamueladi.com>
2024-01-22 00:28:07 +0100

    wrote tests for all use cases

--- test/domain_usecases/search_movies_test.dart
+++ test/domain_usecases/search_movies_test.dart
@@ -3,37 +3,36 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:movie_app/domain/entities/movie.dart';
 import 'package:movie_app/domain/repositories/movies_repository.dart';
-import 'package:movie_app/domain/usecases/get_trending_movies.dart';
+import 'package:movie_app/domain/usecases/search_movies.dart';
 
 import 'get_trending_movies_test.mocks.dart';
 
 @GenerateNiceMocks([MockSpec<MovieRepository>()])
 
-
 void main(){
-  late GetTrendingMovies useCase;
+  late SearchMovies useCase;
   late MockMovieRepository mockMovieRepository;
 
-  setUp(() {
+  setUp((){
     mockMovieRepository = MockMovieRepository();
-    useCase = GetTrendingMovies(mockMovieRepository);
+    useCase = SearchMovies(repository: mockMovieRepository);
   });
 
-  final tMoviesList = [
+  const mQuery = 'Inception';
+
+  final moviesList = [
     Movie(id: 1, title: "Test movies 1", overview: "Desc 1", posterPath: "/image1", year: "2021"),
     Movie(id: 2, title: "Test movies 2", overview: "Desc 2", posterPath: "/image2", year: "2022")
   ];
 
-  test('should get trending movies from the repository', () async{
-    when(mockMovieRepository.getTrendingMovies()).thenAnswer((_) async=> tMoviesList);
+  test('should get a list of queried movies from the repository', () async{
+    when(mockMovieRepository.searchMovies(any)).thenAnswer((realInvocation) async=> moviesList);
 
-    final result = await useCase();
+    final result = await useCase(mQuery);
 
-    expect(result, tMoviesList);
+    expect(result, moviesList);
 
-    verify(mockMovieRepository.getTrendingMovies());
+    verify(mockMovieRepository.searchMovies(mQuery));
     verifyNoMoreInteractions(mockMovieRepository);
   });
-
-  
 }
